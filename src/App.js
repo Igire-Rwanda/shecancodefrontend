84ff56f77a4da0551a009a7691b56813aa55ad15
evening8 <ishimwerichard26@gmail.com>
2023-05-16 10:24:42 +0200

    feature signup page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header/index';
+import Signup from './pages/Signup/index';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Signup />
     </div>
   );
 }
---
+++ src/pages/Signup/index.js
@@ -0,0 +1,9 @@
+let Signup = () =>{
+    return(
+        <div>
+            <h1>helloo signup we are making changes</h1>
+        </div>
+    );
+}
+
+export default Signup;
