f962930ac410f01d7ef152ca3bc8d279d51122ff
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-07-21 16:55:32 +0200

    Update Summer Camp Page

--- src/pages/Courses page/HighSchool.js
+++ src/pages/Courses page/HighSchool.js
@@ -1,36 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Header from "../../components/Header/index";
 import Nav from "../../components/Nav/index";
 import Footer from "../../components/Footers/footer";
 import "./mobileApp.css"
 import Aos from "aos";
 import "aos/dist/aos.css";
-import AccordionContainer from './AccordionContainer';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const HighSchool = () => {
-  const navigate = useNavigate();
-  const [accordions, setAccordions] = useState([ 
-    { 
-      key: 1, 
-      title: 'What is the content of the Summer Coding Camp?', 
-      data: `Our Summer Coding Camp constitutes of mainly 2 parts (Front-end and Back-end) where students with the support of our team of facilitators get to choose a suitable stack that they want.`, 
-      isOpen: false
-    }, 
-    // { 
-    //   key: 2, 
-    //   title: 'Why Mobile Application Development Matters', 
-    //   data: `Mobile applications are the heartbeat of innovation in today's digital landscape. From enhancing user experiences to shaping industries, mobile app development is a dynamic field with boundless possibilities. Embrace the future by acquiring the skills needed to create cutting-edge mobile solutions that address the demands of our fast-paced world.`, 
-    //   isOpen: false
-    // }, 
-    { 
-      key: 2, 
-      title: 'Who Should join?', 
-      data: `We are looking for self-motivated learners eager to uplevel their programming skills and diving deeper into specific tech stacks. We welcome university graduates, secondary school Advanced Level graduates and other young women who are willing to join the software development career path.`, 
-      isOpen: false
-    }, 
-  ]);
-
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
@@ -39,7 +16,7 @@ const HighSchool = () => {
     <div className='w-full flex flex-col justify-start'>
       <Nav />
       <Header />
-      <div className="w-full flex flex-col justify-center items-center">
+      <div className="w-full flex flex-col justify-center items-center mb-10">
         {/* Banner  */}
         <div className='softwareeng-parts-container flex w-full justify-between items-center'>
           <div className='mobileApp-part1 flex flex-col w-11/12 md:w-10/12 m-auto justify-around'>
@@ -50,9 +27,12 @@ const HighSchool = () => {
               Now we are welcoming both <strong>boys</strong> and <strong>girls</strong>.
             </p>
             
-            <a href='https://forms.office.com/r/ixyZS72QVS' className='bg-orange-400 py-2 px-4 mt-3 text-white font-bold mb-3 w-fit md:w-1/5'>Cost: &nbsp;&nbsp;&nbsp;100,000 Rwf</a>
-            <a href='https://forms.office.com/r/ixyZS72QVS' className='mob-btn mb-12 w-full md:w-1/5'>Apply Now</a>
-            {/* <button onClick={() => navigate('https://forms.office.com/r/ixyZS72QVS')} className='mob-btn mb-12 w-full md:w-1/5'>Apply Now</button> */}
+            <a href='https://forms.office.com/r/ixyZS72QVS' className='bg-orange-400 border-l-4 py-2 px-4 mt-3 text-white mb-3 w-full md:w-fit'>
+              <span className='font-bold'>Cost:</span> 
+              <br/>Program fee: 200,000 Rwf
+              <br/>Scholarship: <strong><em>100,000 Rwf (For those who are eligible)</em></strong>  
+            </a>
+            <a href='https://forms.office.com/r/ixyZS72QVS' className='mob-btn mb-12 w-fit'>Apply Now</a>
           </div>
         </div>
 
@@ -67,12 +47,6 @@ const HighSchool = () => {
             <img src='/images/1721462137293.jpeg' alt='' className='w-full'/>
           </Link>
         </div>
-
-        {/* Accordion  */}
-        {/* <div className='flex flex-col w-11/12 md:w-10/12 m-auto justify-around mt-7 bg-white pb-6 md:pb-12'>
-          <h1 className='text-xl md:text-3xl text-left p-0 mb-7'>FAQs</h1>
-          <AccordionContainer accordions={accordions} setAccordions={setAccordions} />
-        </div> */}
       </div>
       <Footer />
     </div>
